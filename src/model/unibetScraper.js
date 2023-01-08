9d97dc31d3d88ad0f1ee3d7d05714a0f9f15de1e
Bram <bramswennen1@gmail.com>
2023-01-08 17:36:03 +0100

    cleanup

--- src/model/unibetScraper.js
+++
@@ -1,4 +0,0 @@
-/*const name =
-  "document.querySelectorAll('.KambiBC-outcomes-list__column')[0].children[1].firstElementChild.textContent";
-const odds =
-  "document.querySelectorAll('.KambiBC-outcomes-list__column')[1].children[1].firstElementChild.firstElementChild.lastElementChild.textContent";*/
